04edb39bff18914f720424001a5d9a56359bf803
Brahim 👨‍💻 <haddad.brahim.59000@gmail.com>
2026-08-23 18:04:04 +0200

    Add GitHub Pages dashboard deployment

---
+++ react_intro/task_4/dashboard/src/Notifications.jsx
@@ -0,0 +1,37 @@
+import closeIcon from './assets/close-icon.png'
+import { getLatestNotification } from './utils.js'
+import './Notifications.css'
+
+function Notifications() {
+  return (
+    <div className="notification-items">
+      <button
+        type="button"
+        aria-label="Close"
+        onClick={() => console.log('Close button has been clicked')}
+        style={{
+          float: 'right',
+          padding: 0,
+          border: 'none',
+          background: 'transparent',
+          cursor: 'pointer',
+        }}
+      >
+        <img src={closeIcon} alt="Close" />
+      </button>
+
+      <p>Here is the list of notifications</p>
+
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        />
+      </ul>
+    </div>
+  )
+}
+
+export default Notifications
